93148b5cbe75dad4258b577d547366549e160ff0
IgorChiv1900 <chiv1900@mail.ru>
2024-07-31 23:39:26 +0300

    Lesson_2

---
+++ src/Park.java
@@ -0,0 +1,9 @@
+public class Park {
+    private int id;
+    private string name;
+    private string timeWork;
+    Public class Address;
+    private string city;
+    private string street;
+    private int houseId;
+}
