0d627cc0e132117aa33fcb67c488197b620696aa
IgorChiv1900 <chiv1900@mail.ru>
2024-07-31 23:56:59 +0300

    Lesson_2

--- src/Main.java
+++ src/Main.java
@@ -12,4 +12,35 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
+
+    public static class Employee {
+            private int id;
+            private String name;
+            private String surname;
+            private String lastName;
+            private String position;
+            private String email;
+            private long phoneNumbers;
+            private long salary;
+            private int years;
+            public Employee (id, name, surname, lastName, position, email, phoneNumbers, salary, years)
+                this.id = id;
+                this.name = name;
+                this.lastName = lastName;
+                this.position = position;
+                this.email = email;
+                this.phoneNumbers = phoneNumbers;
+                this.salary = salary;
+                this.years = years;
+        }
+
+    public static class Person {
+            Person[] persArray = new Person[5];
+            persArray[0] = new Person ("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+            persArray[1] = new Person ("Petrov Petr", "Engineer", "petrovpp@mailbox.com", "892312323", 35000, 33);
+            persArray[2] = new Person ("Petrov Ivan", "Director", "petrovip@mailbox.com", "892313333", 80000, 53);
+            persArray[1] = new Person ("Romanov Vitaly", "Engineer", "petrovvt@mailbox.com", "892314525", 40000, 25);
+            persArray[4] = new Person ("Lity Elena", "Secretary", "lityer@mailbox.com", "892317825", 30000, 27);
+
+    }
 }
--- src/Person.java
+++
@@ -1,9 +0,0 @@
-public class Person {
-        Person[] persArray = new Person[5];
-        persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
-        persArray[1] = new Person("Petrov Petr", "Engineer", "petrovpp@mailbox.com", "892312323", 35000, 33);
-        persArray[2] = new Person("Petrov Ivan", "Director", "petrovip@mailbox.com", "892313333", 80000, 53);
-        persArray[1] = new Person("Romanov Vitaly", "Engineer", "petrovvt@mailbox.com", "892314525", 40000, 25);
-        persArray[4] = new Person("Lity Elena", "Secretary", "lityer@mailbox.com", "892317825", 30000, 27);
-
-}
